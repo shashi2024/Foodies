00ad2c43f7dec551456c9966454429e511d1336b
Sashi2000 <sashinisithara20@gmail.com>
2025-04-02 11:11:19 +0530

    1st update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        axios.get("/api/hello")
+            .then(response => setMessage(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Spring Boot & React App</h1>
+            <p>{message}</p>
+        </div>
+    );
 }
 
 export default App;
